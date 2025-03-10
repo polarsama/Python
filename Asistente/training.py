187aeffddc7041e01a234008d086a75698e9c4a2
David Rincon <andreska9506@gmail.com>
2025-03-10 13:22:09 -0500

    update

---
+++ Asistente/training.py
@@ -0,0 +1,25 @@
+import json
+import random
+import pickle
+import numpy
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.optimizers import sgd_experimental
+
+lemmatizer = WordNetLemmatizer()
+
+intents = json.load(open('intents.json').read)
+
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+
+words = []
+classes= []
+documents = []
+ignore_letters = ['?','!','¿','.',',']
+
