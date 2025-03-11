4e24e0a4d82348eeb2989ef9b7e5fae72488cb33
David Rincon <andreska9506@gmail.com>
2025-03-11 16:24:18 -0500

    update

--- Asistente/training.py
+++ Asistente/training.py
@@ -26,4 +26,33 @@ ignore_letters = ['?','!','¿','.',',']
 for intent in intents['intents']:
     for pattern in intents['patterns']:
         word_list = nltk.word_tokenize(pattern)
-        
+        words.extend(word_list)
+        documents.append((word_list, intent["tag"]))
+        if intent["tag"] not in classes:
+            classes.append(intent["tag"])
+            
+words = [lemmatizer.lemmatize(word) for word in words not in ignore_letters]
+words = sorted(set(words))
+
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
+
+training = []
+output_empty = [0]*len(classes)
+for document in documents:
+    bag = []
+    word_patterns = document[0]
+    word_patterns = [lemmatizer.lemetiz(word.lower()) for word in word_patterns]
+    for word in words:
+        bag.append(1) if word in word_patterns else bag.append(0)
+    output_row = list(output_empty)
+    output_row [classes.index(document[1])] = 1
+    training.append([bag, output_row])
+    
+random.shuffle(training)
+training = np.array(training)
+print(training)
+
+
+train_x = list(training[:,0])
+train_y = list(train[:,1])
