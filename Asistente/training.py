55cb685c342ae1069ef802f3eb387621743c1293
David Rincon <andreska9506@gmail.com>
2025-03-12 17:11:44 -0500

    update V1

--- Asistente/training.py
+++ Asistente/training.py
@@ -1,22 +1,24 @@
 import json
 import random
 import pickle
-import numpy
-import tensorflow 
-import keras # CV y HUB pip install --upgrade keras | keras-hub | keras-cv
+import numpy as np
 
 import nltk
 from nltk.stem import WordNetLemmatizer
 
-from keras.models import Sequential
-from keras.layers import Dense, Activation, Dropout
-from keras.optimizers import sgd_experimental
+
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Dropout
+from tensorflow.keras.optimizers import SGD
 
 lemmatizer = WordNetLemmatizer()
 
-intents = json.load(open('intents.json').read)
-
-nltk.download('punkt')
+with open('intents.json', 'r', encoding='utf-8') as file:
+    intents = json.load(file)
+    
+nltk.download('punkt_tab')
+# nltk.download('punkt')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
 
@@ -26,38 +28,48 @@ documents = []
 ignore_letters = ['?','!','¿','.',',']
 
 for intent in intents['intents']:
-    for pattern in intents['patterns']:
-        word_list = nltk.word_tokenize(pattern)
-        words.extend(word_list)
-        documents.append((word_list, intent["tag"]))
-        if intent["tag"] not in classes:
-            classes.append(intent["tag"])
+    if 'patterns' in intent:
+            for pattern in intent['patterns']:
+                word_list = nltk.word_tokenize(pattern)
+                words.extend(word_list)
+                documents.append((word_list, intent["tag"]))
+                if intent["tag"] not in classes:
+                    classes.append(intent["tag"])
             
-words = [lemmatizer.lemmatize(word) for word in words not in ignore_letters]
+words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
 words = sorted(set(words))
 
 pickle.dump(words, open('words.pkl', 'wb'))
 pickle.dump(classes, open('classes.pkl', 'wb'))
 
 training = []
-output_empty = [0]*len(classes)
+output_empty = [0] * len(classes)
 for document in documents:
     bag = []
     word_patterns = document[0]
-    word_patterns = [lemmatizer.lemetiz(word.lower()) for word in word_patterns]
+    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
     for word in words:
         bag.append(1) if word in word_patterns else bag.append(0)
+    if len(bag) != len(words):
+        print(f"Error: bag tiene longitud {len(bag)}, debería ser {len(words)}")
+    
     output_row = list(output_empty)
-    output_row [classes.index(document[1])] = 1
+    output_row[classes.index(document[1])] = 1
     training.append([bag, output_row])
     
 random.shuffle(training)
-training = np.array(training)
+train_x = []
+train_y = []
+for bag, output_row in training:
+    train_x.append(bag)
+    train_y.append(output_row)
+
+train_x = np.array(train_x)
+train_y = np.array(train_y)
 print(training)
 
 
-train_x = list(training[:,0])
-train_y = list(train[:,1])
+
 
 model = Sequential
 model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
@@ -66,6 +78,6 @@ model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation='softmax'))
 
-sgd = sgd_experimental.SGD(learning_rate=0.001, decay =1e-6, momentum=0.9, nesterov=True)
+sgd = SGD.sgd_experimental(learning_rate=0.001, decay =1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer= sgd, metrics=['accuracy'])
 train_process = model.fit(np.array(train_x), np.array(train_y), epochs=100, batch_size=5, verbose=1)
